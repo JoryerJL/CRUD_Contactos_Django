66d4ae12903814c6a887a4f0e0605e65c4a21143
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-10-26 21:41:22 -0600

    add borrar contacto

--- CRUD_Contactos/gestorContactos/contactos/urls.py
+++ CRUD_Contactos/gestorContactos/contactos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', contactos_list, name = 'contactos_list'),
-    path('crear/', contactos_create, name = 'contactos_create'),
+    path('crear/', contactos_create, name = 'crear'),
+    path('borrar/<int:pk>', contactos_delete, name = 'borrar'),
 ]
--- CRUD_Contactos/gestorContactos/contactos/views.py
+++ CRUD_Contactos/gestorContactos/contactos/views.py
@@ -1,6 +1,6 @@
 from lib2to3.fixes.fix_input import context
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
@@ -20,11 +20,32 @@ def contactos_create(request):
         form = ContactoModelForm(request.POST)
         if form.is_valid():
             form.save()
+            form = ContactoModelForm()
         else:
             print(form.errors)
 
     context = {
         'form': form,
         'title': 'Añadir Contacto'
+    }
+    return render(request, 'contactos/create.html', context)
+
+def contactos_delete(request, pk):
+    Contacto.objects.get(pk=pk).delete()
+    return redirect(contactos_list)
+
+def contactos_edit(request, pk):
+    contacto = Contacto.objects.get(pk=pk)
+    form = ContactoModelForm(instance=contacto)
+    if request.method == 'POST':
+        form = ContactoModelForm(request.POST, instance=contacto)
+        if form.is_valid():
+            form.save()
+            return redirect(contactos_list)
+        else:
+            print(form.errors)
+
+    context = {
+        'form': form,
     }
     return render(request, 'contactos/create.html', context)
